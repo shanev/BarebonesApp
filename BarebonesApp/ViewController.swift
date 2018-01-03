d87d6f441879676789e7bd58f7c4c85a6902b719
Shane Vitarana <shanev@gmail.com>
2018-01-04 01:35:05 +0530

    Created single-view app

---
+++ BarebonesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarebonesApp
+//
+//  Created by Shane Vitarana on 1/4/18.
+//  Copyright © 2018 Shane Vitarana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
