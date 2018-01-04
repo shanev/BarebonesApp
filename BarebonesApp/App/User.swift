e273158f2755ea69e42202f87df83179a32a4114
Shane Vitarana <shanev@gmail.com>
2018-01-04 10:56:03 +0530

    Changed User to use Codable instead of Argo

--- BarebonesApp/App/User.swift
+++ BarebonesApp/App/User.swift
@@ -1,29 +1,12 @@
-public struct User {
-  public let id: String
-  public let deviceToken: String?
-
-  public init(id: String,
-              deviceToken: String? = nil) {
-    self.id = id
-    self.deviceToken = deviceToken
-  }
+struct Device: Codable {
+  let token: String
 }
 
-//extension User: Decodable {
-//  public static func decode(_ json: JSON) -> Decoded<User> {
-//    return curry(User.init)
-//      <^> json <| "_id"
-//      <*> json <|? ["device", "token"]
-//  }
-//}
-//
-//extension User: EncodableType {
-//  public func encode() -> [String : Any] {
-//    var result: [String : Any] = [:]
-//    result["id"] = self.id
-//    result["device"] = ["token": self.deviceToken]
-//
-//    return result
-//  }
-//}
+struct User: Codable {
+  let id: String
+  let device: Device?
 
+  enum CodingKeys: String, CodingKey {
+    case id = "_id"
+  }
+}
